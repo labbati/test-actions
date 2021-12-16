de7705537619c6d100b928507b754668fb879f04
Luca Abbati <luca.abbati@datadoghq.com>
2021-12-16 15:12:16 +0100

    Final example with hard-coded urls

--- .github/workflows/generate-final-artifact.sh
+++ .github/workflows/generate-final-artifact.sh
@@ -28,19 +28,19 @@ curl -L -o $tmp_folder_tracer_archive $tracer_url
 tar -xf $tmp_folder_tracer_archive -C $tmp_folder_tracer
 
 # Bridge folder
-mkdir -p $tmp_folder_final/x86_64-gnu/datadog-library/tracer $tmp_folder_final/x86_64-musl/datadog-library/tracer
-cp -r $tmp_folder_tracer/opt/datadog-php/dd-trace-sources/bridge $tmp_folder_final/x86_64-gnu/datadog-library/tracer
-cp -r $tmp_folder_tracer/opt/datadog-php/dd-trace-sources/bridge $tmp_folder_final/x86_64-musl/datadog-library/tracer
+mkdir -p $tmp_folder_final/x86_64-gnu/dd-library-php/tracer $tmp_folder_final/x86_64-musl/dd-library-php/tracer
+cp -r $tmp_folder_tracer/opt/datadog-php/dd-trace-sources/bridge $tmp_folder_final/x86_64-gnu/dd-library-php/tracer
+cp -r $tmp_folder_tracer/opt/datadog-php/dd-trace-sources/bridge $tmp_folder_final/x86_64-musl/dd-library-php/tracer
 
 # Extension
 php_apis=(20100412 20121113 20131106 20151012 20160303 20170718 20180731 20190902 20200930 20210902)
 for version in "${php_apis[@]}"
 do
-    mkdir -p $tmp_folder_final/x86_64-gnu/datadog-library/tracer/ext/$version $tmp_folder_final/x86_64-musl/datadog-library/tracer/ext/$version
-    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version.so $tmp_folder_final/x86_64-gnu/datadog-library/tracer/ext/$version/datadog-trace.so
-    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version-zts.so $tmp_folder_final/x86_64-gnu/datadog-library/tracer/ext/$version/datadog-trace-zts.so
-    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version-debug.so $tmp_folder_final/x86_64-gnu/datadog-library/tracer/ext/$version/datadog-trace-debug.so
-    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version-alpine.so $tmp_folder_final/x86_64-musl/datadog-library/tracer/ext/$version/datadog-trace.so
+    mkdir -p $tmp_folder_final/x86_64-gnu/dd-library-php/tracer/ext/$version $tmp_folder_final/x86_64-musl/dd-library-php/tracer/ext/$version
+    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version.so $tmp_folder_final/x86_64-gnu/dd-library-php/tracer/ext/$version/datadog-trace.so
+    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version-zts.so $tmp_folder_final/x86_64-gnu/dd-library-php/tracer/ext/$version/datadog-trace-zts.so
+    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version-debug.so $tmp_folder_final/x86_64-gnu/dd-library-php/tracer/ext/$version/datadog-trace-debug.so
+    cp $tmp_folder_tracer/opt/datadog-php/extensions/ddtrace-$version-alpine.so $tmp_folder_final/x86_64-musl/dd-library-php/tracer/ext/$version/datadog-trace.so
 done
 
 ########################
@@ -53,16 +53,16 @@ tar -xf $tmp_folder_profiler_archive -C $tmp_folder_profiler
 php_apis=(20160303 20170718 20180731 20190902 20200930)
 for version in "${php_apis[@]}"
 do
-    mkdir -p $tmp_folder_final/x86_64-gnu/datadog-library/profiler/ext/$version $tmp_folder_final/x86_64-musl/datadog-library/profiler/ext/$version
-    cp $tmp_folder_profiler/datadog-profiling-x86_64-linux/gnu/lib/$version/datadog-profiling.so $tmp_folder_final/x86_64-gnu/datadog-library/profiler/ext/$version/datadog-profiling.so
-    cp $tmp_folder_profiler/datadog-profiling-x86_64-linux/musl/lib/$version/datadog-profiling.so $tmp_folder_final/x86_64-musl/datadog-library/profiler/ext/$version/datadog-profiling.so
+    mkdir -p $tmp_folder_final/x86_64-gnu/dd-library-php/profiler/ext/$version $tmp_folder_final/x86_64-musl/dd-library-php/profiler/ext/$version
+    cp $tmp_folder_profiler/datadog-profiling-x86_64-linux/gnu/lib/$version/datadog-profiling.so $tmp_folder_final/x86_64-gnu/dd-library-php/profiler/ext/$version/datadog-profiling.so
+    cp $tmp_folder_profiler/datadog-profiling-x86_64-linux/musl/lib/$version/datadog-profiling.so $tmp_folder_final/x86_64-musl/dd-library-php/profiler/ext/$version/datadog-profiling.so
 done
 
 ########################
 # Final archives
 ########################
-echo "$release_version" > /tmp/dd-library-php/final/x86_64-gnu/datadog-library/VERSION
+echo "$release_version" > /tmp/dd-library-php/final/x86_64-gnu/dd-library-php/VERSION
 tar -czvf dd-library-php-x86_64-gnu.tar.gz -C /tmp/dd-library-php/final/x86_64-gnu .
 
-echo "$release_version" > /tmp/dd-library-php/final/x86_64-musl/datadog-library/VERSION
+echo "$release_version" > /tmp/dd-library-php/final/x86_64-musl/dd-library-php/VERSION
 tar -czvf dd-library-php-x86_64-musl.tar.gz -C /tmp/dd-library-php/final/x86_64-musl .
